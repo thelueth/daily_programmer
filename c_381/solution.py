9d8de54efe38ca530408adaa4cc2d49b5051490b
thelueth <jonaslueth@gmail.com>
2020-08-11 17:08:40 +0200

    added dice roll generator and solved bonus

--- c_381/solution.py
+++ c_381/solution.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def yahtzee_upper(rolls):
     score = [0] * max(rolls)
     for num in rolls:
@@ -13,3 +15,13 @@ print(max(yahtzee_upper([6, 6, 6, 6, 6])))
 print(max(yahtzee_upper([1654, 1654, 50995, 30864, 1654, 50995, 22747,
     1654, 1654, 1654, 1654, 1654, 30864, 4868, 1654, 4868, 1654,
     30864, 4868, 30864])))
+
+
+def roll_generator(n, start, end):
+    dice = []
+    for i in range(n):
+        dice.append(randint(start,end))
+    
+    return dice
+
+print(max(yahtzee_upper(roll_generator(100000,0,999999))))
