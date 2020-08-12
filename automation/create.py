1606241078e22359a23caafc85b29b9385bcaf2d
thelueth <jonaslueth@gmail.com>
2020-08-12 07:56:35 +0200

    almost done

--- automation/create.py
+++ automation/create.py
@@ -16,7 +16,7 @@ def solved():
     #checks is challenge exists, if true then it pushes the challenge to GitHub
     if os.path.isdir(path):
         os.system("cd %s" % parent_folder)
-        os.system("git add %s" % directory)
+        os.system("git add *")
         os.system("git commit -m 'solved challange %s'" % directory)
         print("Solved and commited %s, need to push to GitHub" % directory)
     else:
