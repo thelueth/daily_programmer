75c612792a45dc5f2d4a3bc42c8da7e9b414bdeb
thelueth <jonaslueth@gmail.com>
2020-08-12 07:41:44 +0200

    solved challange c_100

---
+++ automation/create.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+parent_folder = "/home/jonas/Documents/CTak/daily_programmer"
+directory = "c_" + sys.argv[2]
+path = os.path.join(parent_folder, directory)
+
+def start():
+    #creates a folder with challenge name and creates to empty files
+    os.mkdir(path)
+    open(path+"/challenge.txt",'a').close()
+    open(path+"/solution.py", 'a').close()
+    print("Started Challenge %s" % directory)
+
+def solved():
+    #checks is challenge exists, if true then it pushes the challenge to GitHub
+    if os.path.isdir(path):
+        os.system("git add * && git commit -m 'solved challange %s'" % directory)
+        print("Solved and commited %s, need to push to GitHub" % directory)
+    else:
+        print("%s is not a challenge you started" % directory)
+
+if sys.argv[1] == "start":
+    start()
+elif sys.argv[1] == "solved":
+    solved()
+else:
+    print("Usage: challenge [start/solved] [challenge no]")
